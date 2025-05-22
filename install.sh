d13095789fb0287c32d29f277e1255690fcf398c
kunmo <kmjeong98@gmail.com>
2025-05-22 13:19:40 +0900

    include p10k

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ set -e
 
 CONFIG="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Copy Zsh config
+# Copy Zsh and p10k config
 cp "$CONFIG/zsh/.zshrc" "$HOME/.zshrc"
+cp "$CONFIG/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
 
 # Copy Tmux config
 cp "$CONFIG/tmux/.tmux.conf" "$HOME/.tmux.conf"
@@ -12,6 +13,11 @@ cp "$CONFIG/tmux/.tmux.conf" "$HOME/.tmux.conf"
 # Copy Neovim config
 mkdir -p "$HOME/.config/nvim"
 cp -r "$CONFIG/nvim/"* "$HOME/.config/nvim/"
+
+# Install powerlevel10k into ~/.p10k
+if [ ! -d "$HOME/.p10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.p10k"
+fi
 
 # Remove the config directory itself
 cd ~
--- update.sh
+++ update.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+set -e
 
 CONFIG="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cp ~/.zshrc "$CONFIG/zsh/.zshrc"
-cp ~/.tmux.conf "$CONFIG/tmux/.tmux.conf"
-rsync -a --delete ~/.config/nvim/ "$CONFIG/nvim/"
+# Update Zsh and powerlevel10k config
+cp "$HOME/.zshrc" "$CONFIG/zsh/.zshrc"
+cp "$HOME/.p10k.zsh" "$CONFIG/zsh/.p10k.zsh"
 
+# Update Tmux config
+cp "$HOME/.tmux.conf" "$CONFIG/tmux/.tmux.conf"
+
+# Update Neovim config
+mkdir -p "$CONFIG/nvim"
+rsync -a --delete "$HOME/.config/nvim/" "$CONFIG/nvim/"
+
+# Git commit and push
 cd "$CONFIG"
 git add .
 git commit -m "Update from $(hostname)"
 git push
+
